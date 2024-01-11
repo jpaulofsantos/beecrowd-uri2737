eeb214da8c4339412be03a246a5caf8f7110f4d7
JoaoPaulo <jpaulofsantos@gmail.com>
2024-01-11 17:08:15 -0300

    sql query

--- src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,11 +1,14 @@
 package com.devsuperior.uri2737;
 
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2737.repositories.LawyerRepository;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
@@ -19,7 +22,11 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
 
+		List<LawyerMinProjection> lawyerMinProjectionList = repository.searchSQL();
+
+		for (LawyerMinProjection item: lawyerMinProjectionList) {
+			System.out.println(item.getName() + " - " + item.getCustomersNumber());
+		}
 	}
 }
